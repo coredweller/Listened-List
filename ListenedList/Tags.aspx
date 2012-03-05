786404e3d1ef86df0cc6eb849a226973e6e4dc46
Dan Perillo <dperillo1785@gmail.com>
2012-03-05 00:15:36 -0500

    Tags started
    Started the tag management page.

---
+++ ListenedList/Tags.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tags.aspx.cs" Inherits="ListenedList.Tags"
+    MasterPageFile="~/Masters/Genius.Master" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        Tags
+    </div>
+    <div>
+        Tag Name:
+        <asp:TextBox ID="txtTagName" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSaveTagName" runat="server" OnClick="btnSaveTagName_Click" Text="Save" />
+    </div>
+    <div>
+        <asp:Repeater ID="rptTags" runat="server" OnItemCommand="rptTags_ItemCommand">
+            <ItemTemplate>
+                <p>
+                    <asp:LinkButton CssClass="tag" runat="server" ID="lnkTag" Text='<%# (((Core.DomainObjects.ITag)Container.DataItem)).Name %>'>
+                    </asp:LinkButton>
+                    <asp:LinkButton ID="lnkEdit" runat="server" Text="Edit" CommandName="EDIT" CommandArgument='<%# (((Core.DomainObjects.ITag)Container.DataItem)).Id %>'>
+                    </asp:LinkButton>
+                    <asp:LinkButton ID="lnkDelete" runat="server" Text="Delete" CommandName="DELETE"
+                        CommandArgument='<%# (((Core.DomainObjects.ITag)Container.DataItem)).Id %>'>
+                    </asp:LinkButton>
+                </p>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+</asp:Content>
---
+++ ListenedList/Tags.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Core.Infrastructure;
+using Core.Services.Interfaces;
+
+namespace ListenedList
+{
+    public partial class Tags : ListenedBasePage
+    {
+        ITagService _tagService = Ioc.GetInstance<ITagService>();
+
+        protected void Page_Load( object sender, EventArgs e ) {
+            if ( !IsPostBack ) {
+                Bind();
+            }
+        }
+
+        private void Bind() {
+            var tags = _tagService.GetTags( GetUserId() );
+
+            rptTags.DataSource = tags;
+            rptTags.DataBind();
+        }
+
+        public void btnSaveTagName_Click( object sender, EventArgs e ) {
+            //1. Save the tag name
+            throw new NotImplementedException();
+        }
+
+        public void rptTags_ItemCommand( object source, RepeaterCommandEventArgs e ) {
+
+            var tagId = new Guid( e.CommandArgument.ToString() );
+
+            switch ( e.CommandName.ToLower() ) {
+                case "delete":
+                    DeleteTag( tagId );
+                    break;
+                case "edit":
+                    EditTag( tagId );
+                    break;
+            }
+        }
+
+        private void DeleteTag( Guid tagId ) {
+            
+            var success = false;
+
+            try {
+
+                var tag = _tagService.GetTag( tagId );
+
+                if ( tag == null ) return;
+
+                _tagService.Delete( tag );
+                success = true;
+            }
+            catch ( Exception ex ) {
+                _Log.WriteFatal( "THERE WAS AN ERROR DELETING A TAG ON TAGS.ASPX with message: " + ex.Message );
+                success = false;
+            }
+
+            Bind();
+            ValidateSuccess( success, "You have successfully deleted the tag.", "There was an error deleting the tag." );
+        }
+
+        private void EditTag( Guid tagId ) {
+            //2. Edit tag
+        }
+    }
+}
