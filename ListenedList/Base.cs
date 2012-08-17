adc8b4390098562e158f6ea5e13c0bba712d1ffc
Dan Perillo <dperillo1785@gmail.com>
2012-08-16 23:27:07 -0600

    Base, step 2, tags transformation
    Created a Base to keep functionality that all Base pages need.
    Tags page has now been encapsulated in a control.
    Need to get tutorial of that mode now.

---
+++ ListenedList/Base.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Core.Membership;
+using Core.Helpers.Script;
+
+namespace ListenedList
+{
+    public class Base
+    {
+        protected static IMembershipProvider _MembershipProvider = new ListenedMembershipProvider();
+
+        public static Guid GetUserId( string userName ) {
+
+            var user = _MembershipProvider.GetUser( userName ).ProviderUserKey.ToString();
+            if ( user != null ) {
+                return new Guid( user );
+            }
+
+            return Guid.Empty;
+        }
+
+        public static string ValidateSuccess( bool success, string successMessage, string error ) {
+            PromptHelper prompt;
+
+            if ( success ) {
+                prompt = new PromptHelper( successMessage );
+                return prompt.GetSuccessScript();
+            }
+            else {
+                prompt = new PromptHelper( error );
+                return prompt.GetErrorScript();
+            }
+        }
+    }
+}
